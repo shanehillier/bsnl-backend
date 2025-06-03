8d371175bca378c8240150f0e03c2d4fe61d85ac
shane <shane>
2025-06-03 12:29:00 -0230

    Changed eventsdate preflight

--- api/eventsdate.js
+++ api/eventsdate.js
@@ -6,11 +6,12 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    return res.status(200).end(); // End preflight
+    return res.status(200).end();
   }
 
   const { date } = req.query;
